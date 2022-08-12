a5565e95ee355608adbf59918658f2ba1532142b
Rivaldo Pedro <rivaldoagripino@gmail.com>
2022-08-11 23:22:50 -0300

    Feat: First usecase

---
+++ lib/domain/usecases/authentication.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../entities/account_entity.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth({
+    @required String email,
+    @required String password,
+  });
+}
