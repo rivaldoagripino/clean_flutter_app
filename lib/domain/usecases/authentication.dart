99eaf448f2bd54b3f7fd01b8def00fae3a454440
Rivaldo Pedro <rivaldoagripino@gmail.com>
2022-08-12 20:42:24 -0300

    Feat: Change authentication usecase

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 import '../entities/account_entity.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
-    @required String email,
-    @required String password,
-  });
+  Future<AccountEntity> auth({AuthenticationParams params});
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({@required this.email, @required this.secret});
 }
